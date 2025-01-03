d4b1ef0a66e300a2e39dc4a79cf168bcbcffc3dd
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-03 13:10:31 +0530

    variables,datatypes,operators,typeconversion

---
+++ chapter-02/add.py
@@ -0,0 +1,8 @@
+#Write a python program to add two numbers
+
+num1 = int(input("Enter the number1: "))
+num2 = int(input("Enter the number2: "))
+sum = num1 + num2
+print("Sum of",num1,"and",num2,"is",sum)
+
+# when we use , it automatically adds a space before and at the front of the text..

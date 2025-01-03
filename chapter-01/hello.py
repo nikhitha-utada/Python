201c069d87acc2288da8bec9322c749a9d4e32f8
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-03 11:18:25 +0530

    intro,comments,modules

---
+++ chapter-01/hello.py
@@ -0,0 +1,3 @@
+# This is the code to print the "Hello world" on to the console.
+print("Hello world")
+print("This is my first python program!!!")
---
+++ chapter-01/multiline.py
@@ -0,0 +1,14 @@
+# This is a single line string
+print('Single line')
+
+# This is a multi line string
+print('''Hey this is a multi line
+Where we will use three single qoutes to print the string''')
+
+
+# comments:
+# single line comments
+''' multi line
+    comments  '''
+"""multi line 
+    comments """

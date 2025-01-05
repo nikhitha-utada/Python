5eb6664cf7ccfcbda3bd1193133ac57f5525742a
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-05 20:00:40 +0530

    fstring problem

---
+++ chapter-03/1.py
@@ -0,0 +1,8 @@
+#Write a python program to display a user entered name followed by Good Afternoon using input () function. 
+
+name = input("Enter your name: ")
+print("Good Afternoon",name,"!!!")
+
+#f-strings method --> print(f"text {name}")
+
+print(f"good afternoon {name}")
---
+++ chapter-03/3.py
@@ -0,0 +1,4 @@
+#Write a program to detect double space in a string.
+
+name = "this text contains doube  space  so find it"
+print(name.find("  "))

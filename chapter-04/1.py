b986533ac3b7e9c96b8dc1523943e1511f3dda8b
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-05 23:18:16 +0530

    lists and tuples

---
+++ chapter-04/1.py
@@ -0,0 +1,18 @@
+#Write a program to store seven fruits in a list entered by the user.
+
+fruits = []
+f1 = input("enter the fruit name: ")
+fruits.append(f1)
+f1 = input("enter the fruit name: ")
+fruits.append(f1)
+f1 = input("enter the fruit name: ")
+fruits.append(f1)
+f1 = input("enter the fruit name: ")
+fruits.append(f1)
+f1 = input("enter the fruit name: ")
+fruits.append(f1)
+f1 = input("enter the fruit name: ")
+fruits.append(f1)
+f1 = input("enter the fruit name: ")
+fruits.append(f1)
+print(fruits)

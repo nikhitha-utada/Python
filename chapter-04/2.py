2182547ab13b7e47247b789833b815a3b114fa30
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-05 23:17:28 +0530

    lists and tuples

---
+++ chapter-04/2.py
@@ -0,0 +1,19 @@
+# Write a program to accept marks of 6 students and display them in a sorted manner. 
+
+marks = []
+marks1 = int(input("enter student marks: "))
+marks.append(marks1)
+marks1 = int(input("enter student marks: "))
+marks.append(marks1)
+marks1 = int(input("enter student marks: "))
+marks.append(marks1)
+marks1 = int(input("enter student marks: "))
+marks.append(marks1)
+marks1 = int(input("enter student marks: "))
+marks.append(marks1)
+marks1 = int(input("enter student marks: "))
+marks.append(marks1)
+marks1 = int(input("enter student marks: "))
+marks.append(marks1)
+marks.sort()
+print(marks)

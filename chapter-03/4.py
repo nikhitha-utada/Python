c2c6dd21c8a2a329ea1a6fd8c4737b4747bc0fab
Nikhitha Utada <128949572+nikhitha-utada@users.noreply.github.com>
2025-01-05 20:48:20 +0530

    Add files via upload

---
+++ chapter-03/4.py
@@ -0,0 +1,5 @@
+#Replace the double space from problem 3 with single spaces.
+
+name = "this text contains doube  space  so find it"
+replaced_text = name.replace("  "," ")
+print(name,"after replacing double space with single space is",replaced_text)
---
+++ chapter-03/5.py
@@ -0,0 +1,4 @@
+#Write a program to format the following letter using escape sequence characters
+
+letter = "Dear Harry,\n this python course is nice. Thanks!"
+print(letter)  #added \n to add a new line

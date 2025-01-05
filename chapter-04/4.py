189445316f9375e1ca98bc05a856aecfcdf5f9ef
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-05 23:19:59 +0530

    lists and tuples

---
+++ chapter-04/4.py
@@ -0,0 +1,7 @@
+# Write a program to sum a list with 4 numbers.
+ 
+nums = [1,2,3,4]
+#sum = nums[0] + nums[1] + nums[2] + nums[3]
+#print(sum)
+
+print(sum(nums)) #sum is a built in function of lists

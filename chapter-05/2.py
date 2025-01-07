dc07e71d9ded2f78d1c4f5a4b1ae6f9cd2071782
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-07 19:26:33 +0530

    dictionaries and  sets

---
+++ chapter-05/2.py
@@ -0,0 +1,23 @@
+#Write a program to input eight numbers from the user and display all the unique numbers (once). 
+
+set = set()
+num = int(input("enter the number: "))
+set.add(num)
+num = int(input("enter the number: "))
+set.add(num)
+num = int(input("enter the number: "))
+set.add(num)
+num = int(input("enter the number: "))
+set.add(num)
+num = int(input("enter the number: "))
+set.add(num)
+num = int(input("enter the number: "))
+set.add(num)
+num = int(input("enter the number: "))
+set.add(num)
+num = int(input("enter the number: "))
+set.add(num)
+print(f"The set is {set}")
+print(f"The set is {set}")
+
+

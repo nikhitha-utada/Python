cdbc6b6fdbb8932012c16fa98dc9e30026013383
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-07 22:14:35 +0530

    conditional expressions

--- chapter-06/3.py
+++ chapter-06/3.py
@@ -1,5 +1,7 @@
 #A spam comment is defined as a text containing following keywords: “Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program to detect these spams.
 
+#we can use in keyword as well....
+
 sentence = input("Enter the sentence that you want to check for the presence of spam comment: ")
 spam = sentence.find("Make a lot of money" or "buy now" or "subscribe this" or "click this") #find() finds for a substring if it is present in the string it returns the index else returns -1.
 if(spam == -1):

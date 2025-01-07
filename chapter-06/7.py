10db8bb5fff7bba4f6a13eb0b08a071b6462c0b9
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-07 22:12:28 +0530

    conditional expressions

---
+++ chapter-06/7.py
@@ -0,0 +1,7 @@
+#Write a program to find out whether a given post is talking about “Harry” or not. 
+
+post = input("enter the post content: ")
+if("Harry" in post):
+    print("yes")
+else:
+    print("no")

87bf51b46384848062a2686d0167e76c9820a3f9
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-11 23:41:42 +0530

    swapcase

---
+++ easy-problems/10.py
@@ -0,0 +1,11 @@
+# Write a program to convert all the upper case letters in the given string to  lower case letter and vice versa.  
+# Testcase1 : JohnWick  
+# Output : jOHNwICK  
+# Explanation : All the upper case letters changed to lower case and vise  versa.  
+# Testcase1 : Korean  
+# Output : kOREAN  
+# Explanation : All the upper case letters changed to lower case and vise  versa.
+
+#Method 1:
+str = input("enter a string: ")
+print(str.swapcase())

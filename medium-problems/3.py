2df9ba2dbd945e13e742618b3970c54aa41c1b04
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-12 10:51:37 +0530

    added end param

--- medium-problems/3.py
+++ medium-problems/3.py
@@ -5,6 +5,7 @@
 # Explanation : In the given array of number [ 7, 4, 7, 23, 10 ] the numbers 4 and 10 by adding one to these numbers, they formed prime number 5 and 11. So the output is 4 10.
 
 arr = []
+answer = []
 n = int(input("enter the number of elements in an array: "))
 for i in range(n):
     num = int(input("Enter the element: "))
@@ -17,5 +18,7 @@ for num in arr:
             is_prime = False    #even if 1 out of that range is divisible then it is not prime so it should be set to false
             break
     if is_prime:
-        print(num)
+        answer.append(num)
+for i in answer:
+    print(i,end=" ")
 

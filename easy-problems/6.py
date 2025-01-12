d6b659a39cb945ee8fd59abdce4d3f5f008a8161
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-12 10:59:38 +0530

    digits in-between the first and last # digit are less than first and last digit

--- easy-problems/6.py
+++ easy-problems/6.py
@@ -8,29 +8,29 @@
 # Explanation : The middle digits 4,7,1 are less than first digit 8 and last  digit 9 .  
 
 #method-1
-# num=int(input("Enter the number:"))
-# temp=num
-# last=num%10
-# count=0
-# while temp>0:
-#     temp=temp%10
-#     count+=1
-#     temp//=10
-# first=num//10**(count-1)
-# digit=str(num)
-# mid=digit[1:((len(digit))-1)]
-# remain=int(mid)
-# answer=True
-# for i in mid:
-#     remlast=remain%10
-#     if remlast<=first and remlast<=last:
-#         answer=True
-#         # print(true)
-#     else:
-#         answer=False
-#     # remain//=10
-#     # print(false)
-# print(answer)
+num=int(input("Enter the number:"))
+temp=num
+last=num%10
+count=0
+while temp>0:
+    temp=temp%10
+    count+=1
+    temp//=10
+first=num//10**(count-1)
+digit=str(num)
+mid=digit[1:((len(digit))-1)]
+remain=int(mid)
+answer=True
+for i in mid:
+    remlast=remain%10
+    if remlast<=first and remlast<=last:
+        answer=True
+        # print(true)
+    else:
+        answer=False
+    # remain//=10
+    # print(false)
+print(answer)
 
 #method-2
 num=int(input("Enter a number :"))     

4a9cac2526f4ce6ce751d091e0c64b1b6096c5e6
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-16 20:46:40 +0530

    convert into title case

---
+++ easy-problems/25.py
@@ -0,0 +1,13 @@
+# Convert String to Title Case 
+# Question: Write a function that converts a string to title case (capitalize 
+# the first letter of each word). 
+# Testcase: "hello world" 
+# Output: "Hello World" 
+# Explanation: The first letter of each word is capitalized, resulting in "Hello 
+# World".
+
+#method-1:
+str=input("enter the string: ")
+str2=str.title()
+print(str2)
+

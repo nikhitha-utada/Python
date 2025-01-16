27a4a96d492dfc8902e86e11f8d4989a634e3b7c
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-16 20:50:28 +0530

    convert string to int method-1

---
+++ easy-problems/26.py
@@ -0,0 +1,11 @@
+# Convert String to Number 
+# Question: Write a function to convert a string to a number (without using 
+# parseInt or Number). 
+# Testcase: "123" 
+# Output: 123 
+# Explanation: The string "123" is converted to the number 123. 
+
+#method-1:
+num = int(input("enter the number: "))  #input() converts string to int
+print(type(num))
+

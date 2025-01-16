a978cc706e5fb5e742e813dcc1b379129f81e861
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-16 16:28:31 +0530

    reversing array method-2

--- easy-problems/17.py
+++ easy-problems/17.py
@@ -6,10 +6,21 @@
 
 
 #method-1:
+# n = int(input("enter the number of elements: "))
+# arr=[]
+# for i in range(0,n):
+#     element = int(input("enter the element: "))
+#     arr.append(element)
+
+# print(arr[::-1])
+
+#method-2:
 n = int(input("enter the number of elements: "))
 arr=[]
+lst=[]
 for i in range(0,n):
     element = int(input("enter the element: "))
     arr.append(element)
-
-print(arr[::-1])
+for i in range(len(arr)-1,-1,-1):
+    lst.append(arr[i])
+print(lst)

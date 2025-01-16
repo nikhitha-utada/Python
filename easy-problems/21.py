e59868a1f05b077021d7f42b10aea28de5b60bf2
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-16 20:05:30 +0530

    reverse of string method-1

---
+++ easy-problems/21.py
@@ -0,0 +1,11 @@
+# Reverse a String :  
+# Question: Write a function to reverse a given string. 
+# Testcase: "hello" 
+# Output: "olleh" 
+# Explanation: The reverse of the string "hello" is "olleh". Each character's 
+# order is reversed.
+
+# method-1:
+
+str = input("enter the string: ")
+print(str[::-1])

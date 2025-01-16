6e060a8db341a0b6a4fae82a3d8ed00d08bc0d20
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-16 16:21:30 +0530

    reversing array method-1

---
+++ easy-problems/17.py
@@ -0,0 +1,15 @@
+#  Reverse an Array  
+# Problem: Write a function to reverse the elements in an array.  
+# Testcase 1:  
+# Input: [1, 2, 3, 4, 5]  
+# Output: [5, 4, 3, 2, 1]  
+
+
+#method-1:
+n = int(input("enter the number of elements: "))
+arr=[]
+for i in range(0,n):
+    element = int(input("enter the element: "))
+    arr.append(element)
+
+print(arr[::-1])

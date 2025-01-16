7b6613e3c2d83b3492e0be3091803767458a0b55
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-16 21:57:40 +0530

    prevent a python print() function to print a new line at the end.

---
+++ chapter-08/2.py
@@ -0,0 +1,9 @@
+# How do you prevent a python print() function to print a new line at the end.
+
+print("hello",end=" ")
+print("world")
+
+#end="" parameter is used to control how we print on to the console..
+#by default --> \n prints on new line
+# end = " " --> creates a whitespace
+#end = "" --> same line with no space

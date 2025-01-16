6a2f1f7e30a4405cb87f41cc4e286485f4c5e7cf
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-16 21:07:42 +0530

    checks if string contains only digits  method-1

---
+++ easy-problems/27.py
@@ -0,0 +1,18 @@
+# Check if String Contains Only Digits 
+# Question: Write a function to check if a string contains only numeric 
+# digits. 
+# Testcase: "12345" 
+# Output: true 
+# Explanation: The string "12345" consists only of digits, so the result is 
+# true.
+
+#method-1:
+num  = input("enter the number: ")
+is_digit = False
+for i in num:
+    if i.isdigit():
+        is_digit = True
+    else:
+        is_digit = False
+        break
+print(is_digit)

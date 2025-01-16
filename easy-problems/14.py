4d65e65cd1629d0033090044e3a2f8c94ef819e3
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-16 15:06:40 +0530

    sum of elements of array

---
+++ easy-problems/14.py
@@ -0,0 +1,16 @@
+# Sum of All Elements  
+# Problem: Write a function that returns the sum of all elements in an array.  
+# Testcase 1:  
+# Input: [1, 2, 3, 4]  
+# Output: 10 
+# Explanation:  
+# The sum of the elements 1 + 2 + 3 + 4 = 10.  
+
+# method-1:
+n = int(input("enter the number of elements: "))
+arr=[]
+for i in range(0,n):
+    element = int(input("enter the element: "))
+    arr.append(element)
+print(sum(arr))
+

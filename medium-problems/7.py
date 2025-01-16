96150979824855c8a3ca5bdb7d56b40ad7a4c1db
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-16 10:54:41 +0530

    finding second duplicate

---
+++ medium-problems/7.py
@@ -0,0 +1,15 @@
+# Write a program that takes number of rows as input and print below respective pattern.
+
+# Testcase1	:  Enter number of rows: 4
+# Output     	: 
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+
+num=int(input("Enter number of rows: "))
+for row in range(1,num+1):
+    for col in range(1,row+1):
+        print(col,end=" ")
+    print()

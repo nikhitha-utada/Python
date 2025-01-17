5cc9a5376fbbb87d002bcdd7a12fbb7f3891ea43
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-17 12:26:18 +0530

    added time and space complexities

--- DSA/linear_search.py
+++ DSA/linear_search.py
@@ -1,7 +1,11 @@
 # checks the key with each and every element of the array from the beginning till the end..if element is found it return the index of the first occurence of that element or if the element is not found it returns -1
 # key --> element to be searched
 # linear search is also called as sequential search
-
+#Time complexity:
+    # Best case scenario: O(1)
+    # Average case scenario : O(n)
+    # Worst case scenario : O(n)
+#space complexity: O(1)
 
 #algorithm:
 

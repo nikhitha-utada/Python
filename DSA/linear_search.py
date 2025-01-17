3ff507617fcf5c84c091b7aca71c3fd3faceace2
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-17 12:09:57 +0530

    linear search algorithm

---
+++ DSA/linear_search.py
@@ -0,0 +1,22 @@
+# checks the key with each and every element of the array from the beginning till the end..if element is found it return the index of the first occurence of that element or if the element is not found it returns -1
+# key --> element to be searched
+# linear search is also called as sequential search
+
+
+#algorithm:
+
+#method-1:
+def linear_search(arr,key): 
+    for i in range(0,len(arr)):
+        if arr[i]==key:
+            return i   
+    return -1
+
+n = int(input("enter the number of elements: "))
+arr=[]
+for i in range(0,n):
+    element = int(input("enter the element of the array: "))
+    arr.append(element)
+key = int(input("Enter the key to be searched: "))  
+print(linear_search(arr,key))  
+

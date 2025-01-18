df24937891456f4ae685ccc14bb9deab7b23b2da
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-18 15:51:21 +0530

    programmer information

---
+++ oops/1.py
@@ -0,0 +1,15 @@
+class Programmer:
+    company = "microsoft"
+    def __init__(self,name,role,salary):    # constructor function to initialize the object values
+        self.name = name
+        self.role = role
+        self.salary = salary
+
+emp1 = Programmer("Nikhitha","SDE",200000)  # passing values to the constructor
+print(emp1)
+print(emp1.name)
+print(emp1.role)
+print(emp1.salary)
+print(Programmer.company)
+print(emp1.company)
+    

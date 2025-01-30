0880d602c8fd534a257e139148fa3413ada3a9af
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-30 22:53:06 +0530

    rotating array method-1

---
+++ medium-problems/array_rotation.py
@@ -0,0 +1,20 @@
+# Rotate an Array
+
+# Problem: Write a function that rotates an array to the right by a given number of steps.
+# Testcase 1:
+# Input: [1, 2, 3, 4, 5], 2
+# Output: [4, 5, 1, 2, 3]
+
+num = int(input("enter the number of elements: "))
+lst = []
+for i in range(0,num):
+    ele = int(input("enter the element: "))
+    lst.append(ele)
+k = int(input("enter the k value: "))
+for i in range(0,k):
+    lst.insert(0,lst[-1])    # adding that element at the start of the list and removing the element at the same time
+    # del lst[-1]
+    lst.pop(-1)
+print(lst)
+
+
